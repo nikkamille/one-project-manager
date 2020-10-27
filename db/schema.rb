906d561ef6adeb13bf9892a9852ba46a7ee278a9
nikkamille <k.pagibigan@gmail.com>
2020-10-27 03:25:12 -0600

    adds column to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_26_090726) do
+ActiveRecord::Schema.define(version: 2020_10_27_092307) do
 
   create_table "projects", force: :cascade do |t|
     t.string "title"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 2020_10_26_090726) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "admin"
   end
 
   add_foreign_key "tasks", "projects"
