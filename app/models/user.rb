2b235fbe45c37935dc30a7db1f6e5775daa968b6
nikkamille <k.pagibigan@gmail.com>
2020-10-29 01:06:36 -0600

    adds another validation to the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
     has_secure_password
-    validates :username, presence: true
-    validates :email, presence: true
+    validates :username, presence: true, uniqueness: true
+    validates :email, presence: true, uniqueness: true
     validates :password, presence: true
     validates :password_confirmation, presence: true
     has_many :projects
