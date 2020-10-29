76e2715d973b1f454be98292d38b3e749203902d
nikkamille <k.pagibigan@gmail.com>
2020-10-28 21:31:19 -0600

    creates projects index view, adds methods to projects controller, adds validations to project forms

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,4 +4,26 @@ class ProjectsController < ApplicationController
         @projects = Project.all
     end
 
+    def new
+        @project = Project.new
+    end
+
+    def create
+        @project = Project.new(project_params)
+
+        if @project.save
+            redirect_to project_path(@project)
+        else
+            render :new
+        end
+    end
+
+
+
+    private
+
+    def project_params
+        params.require(:project).permit(:title)
+    end
+
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,5 @@
 class Project < ApplicationRecord
     has_many :tasks
     has_many :users, through: :tasks
+    validates :title, presence: true
 end
