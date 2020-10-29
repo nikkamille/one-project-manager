c3778286f8591eb82314abf0c873d41c4ffcc065
nikkamille <k.pagibigan@gmail.com>
2020-10-29 00:58:03 -0600

    adds dropdown for the form and adds a constant to the Task model for a fixed array of options

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,4 +4,8 @@ class Task < ApplicationRecord
   validates :name, presence: true
   validates :description, presence: true
   validates :status, presence: true
+  
+  STATUS_TYPES = ["Not Started", "In Progress", "Completed"]
+  
+
 end
