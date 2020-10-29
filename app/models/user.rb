58a2beda513c973a2cb816de06d14f05d5fda99e
nikkamille <k.pagibigan@gmail.com>
2020-10-29 02:14:20 -0600

    updates relationship between models

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
     validates :email, presence: true, uniqueness: true
     validates :password, presence: true
     validates :password_confirmation, presence: true
-    has_many :projects
-    has_many :tasks, through: :projects
+    has_many :tasks
+    has_many :projects, through: :tasks
 end
