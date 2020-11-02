f4cfaf2621c421f3a6a3c6ea9892a49c40ca2a68
nikkamille <k.pagibigan@gmail.com>
2020-11-01 19:34:03 -0700

    updates the task_params, now accepts a user's username instead of a user id; updates the new form so that the project manager or admin can find a user using a username instead of a user id

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -33,7 +33,7 @@ class TasksController < ApplicationController
     private
 
     def task_params
-        params.require(:task).permit(:project_title, :name, :description, :status, :user_id)
+        params.require(:task).permit(:project_title, :name, :description, :user_username, :status)
     end
 
 end
