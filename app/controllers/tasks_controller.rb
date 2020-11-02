2a29a7cd6053893209db72a3d352d38fc9675c01
nikkamille <k.pagibigan@gmail.com>
2020-11-02 03:20:02 -0700

    adds edit method to task controller; creates edit form in view; adds edit button to task show page

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -22,6 +22,19 @@ class TasksController < ApplicationController
         @task = Task.find_by(id: params[:id])
     end
 
+    def edit
+        @task = Task.find_by(id: params[:id])
+    end
+
+    def update
+        @task = Task.find_by(id: params[:id])
+        if @task.update(task_params)
+            redirect_to task_path(@task)
+        else
+            render :edit
+        end
+    end
+
 
     def destroy
         @task = Task.find(params[:id]).destroy
