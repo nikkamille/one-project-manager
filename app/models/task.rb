c95c172e3c3016e15d434c07627f13dd5393ad23
nikkamille <k.pagibigan@gmail.com>
2020-11-01 18:50:32 -0700

    adds a custom setter and getter method project_title to the the task model

--- app/models/task.rb
+++ app/models/task.rb
@@ -6,6 +6,14 @@ class Task < ApplicationRecord
   validates :status, presence: true
   
   STATUS_TYPES = ["Not Started", "In Progress", "Completed"]
+
+  def project_title=(title)
+    self.project = Project.find_or_create_by(title: title)
+  end
+
+  def project_title
+    self.project ? self.project.title : nil
+  end
   
 
 end
