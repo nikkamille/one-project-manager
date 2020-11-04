6d34e984e8c5cca3bd25cc35fd04fa0995a5acd8
nikkamille <k.pagibigan@gmail.com>
2020-11-03 23:16:50 -0700

    adds validation to login method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,16 +4,17 @@ class SessionsController < ApplicationController
     end
  
     def create
-        if @user = User.find_by(username: params[:username])
+        @user = User.find_by(username: params[:username])
+        if @user && @user.authenticate(params[:password])
             session[:user_id] = @user.id
             redirect_to user_path(@user)
         else
-            render 'new'
+            redirect_to "/login", alert: "Username and/or password is incorrect."
         end
     end
 
     def destroy
         session.delete("user_id")
-        redirect_to root_path
+        redirect_to "/home"
     end
 end
